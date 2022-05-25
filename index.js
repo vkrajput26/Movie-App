c28bc27ccfd08e74a33f93dd2c5334b7cc9b995a
Vivek Rajput <indvivekrajput@gmail.com>
2022-05-25 22:59:35 +0530

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //https://www.omdbapi.com/?apikey=9b3377fb&t=thor
 //https://www.omdbapi.com/?apikey=9b3377fb&t=thor
-
+let not=document.getElementById("not-found")
 
 let container=document.getElementById("container")
 function movies(){
@@ -16,16 +16,28 @@ function movies(){
     })
     .catch(function(err){
         console.log(err)
-        let not=document.getElementById("not-found")
-        let img=document.createElement("img");
-        img.src="https://media2.giphy.com/media/C21GGDOpKT6Z4VuXyn/200w.webp?cid=ecf05e475iqeq3h19kd0zqx1w6y1gpg9yog61xigjj89cimk&rid=200w.webp&ct=g"
-   
-        not.append(img)
+      
     })
 }
 
     function append(data){
+        console.log(data)
+       container.innerHTML=null
+       not.innerHTML=null
+           
+            if(data==undefined)
+            {
+              
+                let img=document.createElement("img");
+                img.src="https://media2.giphy.com/media/C21GGDOpKT6Z4VuXyn/200w.webp?cid=ecf05e475iqeq3h19kd0zqx1w6y1gpg9yog61xigjj89cimk&rid=200w.webp&ct=g"
+           
+                not.append(img)
+            }
+        
         data.forEach(function(el){
+
+
+          
             console.log(el)
             data=null
             let box=document.createElement("div");
@@ -34,6 +46,9 @@ function movies(){
 
             let title=document.createElement("p")
             title.innerText=el.Title;
+            title.addEventListener("click",function(){
+                myfun()
+            })
             title.style.textAlign="center"
             title.setAttribute("id","title")
 
@@ -47,4 +62,10 @@ function movies(){
             container.append(box)
             
         })
+    
     }
+
+  function  myfun()
+  {
+      console.log("myfun")
+  }
